033bbe6fb5c08d260271e4fc0df3efdf580e00eb
João Rafael <joaorafael2502@gmail.com>
2023-05-22 16:35:07 -0300

    feat: api access module, requirement 01

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,15 @@
 export async function getCategories() {
-  // Implemente aqui
+  const URL = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const resultApi = await fetch(URL);
+  const data = await resultApi.json();
+  return data;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const URL_2 = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const resultApi = await fetch(URL_2);
+  const data = await resultApi.json();
+  return data;
 }
 
 export async function getProductById() {
