cfbefbe32631043cb5c74d607883f052a7d51ffa
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-05-22 16:53:57 -0300

    feat: create homepage, req 02

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 // Primeiro Commit
 import React from 'react';
 import { getCategories } from './services/api';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
-
+    <Switch>
+      <Route exact path="/" component={ Home } />
+    </Switch>
   );
 }
 
