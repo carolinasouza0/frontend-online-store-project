d98e936f371dc5d38f16dce6cec4efa3b0bbda23
Bruno <bruno_bury@hotmail.com>
2023-05-22 17:29:11 -0300

    feat: creat shopping cart, req 03

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 // Primeiro Commit
 import React from 'react';
-import { getCategories } from './services/api';
+// import { getCategories } from './services/api';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
+import ShoppingCart from './components/ShoppingCart';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Home } />
+      <Route exact path="/shoppingCart" component={ ShoppingCart } />
     </Switch>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,32 @@
-import { Component } from "react";
+import { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p> 
+
+        <div>
+          <p data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+        </div>
+
+        <div>
+          <button
+            type="button"
+          >
+            <Link
+              to="/shoppingCart"
+              data-testid="shopping-cart-button"
+            >
+              carrinho
+            </Link>
+          </button>
+        </div>
       </div>
-    )
+
+    );
   }
 }
 
